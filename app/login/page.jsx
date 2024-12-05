9a4344422b5a502d9881d81fdd414d77b29afb6d
Kavi8428 <lakshanudara987@gmail.com>
2024-12-06 00:12:03 +0530

    Add loging page

---
+++ app/login/page.jsx
@@ -0,0 +1,8 @@
+
+export default function logingPage() {
+  return (
+    <div className="bg-orange-400 justify-center items-center min-h-screen flex flex-col text-center " >
+      <h1 className=" bg-slate-700 rounded text-center p-2" >Welcome to Login</h1>
+    </div>
+  )
+}
