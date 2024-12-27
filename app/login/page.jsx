b1973dca759356e033ded8d0e7b59a608ce14dff
Kavi8428 <lakshanudara987@gmail.com>
2024-12-27 23:44:55 +0530

    Add front end validation to inputs

--- app/login/page.jsx
+++ app/login/page.jsx
@@ -1,8 +1,11 @@
+import LoginForm from "./login-form";
 
 export default function logingPage() {
-  return (
-    <div className="bg-orange-400 justify-center items-center min-h-screen flex flex-col text-center " >
-      <h1 className=" bg-slate-700 rounded text-center p-2" >Welcome to Login</h1>
-    </div>
-  )
+    return (
+        <div>
+            <div className="flex flex-col justify-center min-h-screen items-center ">
+                <LoginForm/>
+            </div>
+        </div>
+    )
 }
