f309c62112d0beded27740bf076b9bcf1eb34d58
Kavi8428 <lakshanudara987@gmail.com>
2025-01-14 15:03:34 +0530

    Registration process done

--- app/libs/apis/server.js
+++ app/libs/apis/server.js
@@ -1,43 +1,58 @@
-import { api } from "../api";
+import { api } from '../api'
 
-const loginUser = async (data)=>{
+const loginUser = async data => {
+  console.log('SentData', data)
 
-    console.log('SentData',data);
+  let response = await fetch('http://localhost:3000/api/v1/login', {
+    method: 'POST',
+    body: JSON.stringify({
+      email: data.email,
+      password: data.password
+    })
+  })
+  console.log('API response to server', await response.json())
+}
+export default loginUser
 
-    
-   let response = await fetch('http://localhost:3000/api/v1/login',{
-        method: 'POST',
-        body: JSON.stringify({
-            email: data.email,
-            password: data.password
-        })
-    });
-    console.log('API response to server',await response.json());
-    };
+export const register = async form_data => {
+  try {
+    const response = await api.post('register', { json: form_data });
+    if(!response.ok){
+        throw new Error('Error in endpoint');
+    }
+    return response.json();
 
-
-    
-
-export default loginUser;
+  } catch (error) {
+    const status = error.response.status;
+    if(status && status == 409){
+        return {
+            error : 'This email with user already exist'
+        }
+    }else if(status && status == 500)
+   // console.log('Error occured while connecting register endpoint ', error);
+    return {
+        error : 'Internal server error'
+    }
+  }
+}
 
 export const getMovies = async () => {
-    try{
-        const response = await api.get("movies");
-        return response.json();
-
+  try {
+    const response = await api.get('movies')
+    return response.json()
+  } catch (error) {
+    if (error.response) {
+      // The request was made and the server responded with a status code
+      // that falls out of the range of 2xx
+      console.error('Error status:', error.response.status)
+      console.error('Error data:', error.response.data)
+    } else if (error.request) {
+      // The request was made but no response was received
+      console.error('No response received:', error.request)
+    } else {
+      // Something happened in setting up the request that triggered an Error
+      console.error('Error message:', error.message)
     }
-    catch(error){
-        if (error.response) {
-            // The request was made and the server responded with a status code
-            // that falls out of the range of 2xx
-            console.error('Error status:', error.response.status);
-            console.error('Error data:', error.response.data);
-        } else if (error.request) {
-            // The request was made but no response was received
-            console.error('No response received:', error.request);
-        } else {
-            // Something happened in setting up the request that triggered an Error
-            console.error('Error message:', error.message);
-        }
-        console.error('Error config:', error.config);    }
-  };
+    console.error('Error config:', error.config)
+  }
+}
